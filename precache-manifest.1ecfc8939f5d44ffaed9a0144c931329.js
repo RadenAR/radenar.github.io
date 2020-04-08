c1bcee870e624b0cd210b262bb74400cfc6973f7
RadenAR <radenabdulrahman@gmail.com>
2020-04-08 15:35:13 -0400

    Updates

---
+++ precache-manifest.1ecfc8939f5d44ffaed9a0144c931329.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8873ebc4924f4fa0f1daf0bdf5b8f36",
+    "url": "/index.html"
+  },
+  {
+    "revision": "339c32491f2bcca18af8",
+    "url": "/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "73196caae9eff29fc936",
+    "url": "/static/js/2.5b82c3d8.chunk.js"
+  },
+  {
+    "revision": "bc51ad6b075e5b96cf9ad2ad3d22ac0d",
+    "url": "/static/js/2.5b82c3d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "339c32491f2bcca18af8",
+    "url": "/static/js/main.b1883c64.chunk.js"
+  },
+  {
+    "revision": "8d038be3506856613b65",
+    "url": "/static/js/runtime-main.9df3c142.js"
+  }
+]);
